3dc51b4f1399f33a646e72c9221ed818e88794aa
jin-hyoung,Lee <jhcode0@naver.com>
2025-06-30 14:19:04 +0900

    feat: apex-charts 커스텀 - 가장 많은 접속자 시간대 bar 색상 변경 - 모바일 에서는 현재 시간 기준 +- 1시간 (총 3시간) 그래프만 보이도록

--- src/app/(admin)/admin/dashboard/page.tsx
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -1,7 +1,32 @@
 "use client";
 import VisitorChart from "@/component/admin/dashboard/VisitorChart";
 import visitor from "@/data/visitor.json";
+import { useEffect, useState } from "react";
+
 export default function AdminDashboard() {
+  const [winSize, setWinSize] = useState<number>(0);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const viewportSizeFunc = () => {
+    const viewportSize = window.innerWidth;
+    setWinSize(viewportSize);
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      viewportSizeFunc();
+      window.addEventListener("resize", viewportSizeFunc);
+      return () => {
+        window.removeEventListener("resize", viewportSizeFunc);
+      };
+    }
+  }, []);
+  useEffect(() => {
+    if (winSize > 767) {
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+    }
+  }, [winSize]);
+
   return (
     <>
       <div className="dashboardWrAP w-full p-4 flex justify-center items-center flex-col">
@@ -9,7 +34,10 @@ export default function AdminDashboard() {
           <h2 className="w-full py-8 text-[1.5rem] text-center">
             관리자 대시보드
           </h2>
-          <VisitorChart data={visitor} />
+          <div>
+            <h3 className="text-[1rem] text-center py-2">일간 방문자</h3>
+            <VisitorChart data={visitor} isMobile={isMobile} />
+          </div>
           <div></div>
           <div></div>
         </div>
--- src/component/admin/dashboard/VisitorChart.tsx
+++ src/component/admin/dashboard/VisitorChart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useMemo } from "react";
 import dynamic from "next/dynamic";
 import { ApexOptions } from "apexcharts";
 
@@ -7,25 +7,58 @@ const ReactApexChart = dynamic(() => import("react-apexcharts"), {
 });
 
 interface VisitorData {
-  hour: string;
+  hour: number;
   count: number;
 }
-export default function VisitorChart({ data }: { data: VisitorData[] }) {
+export default function VisitorChart({
+  data,
+  isMobile,
+}: {
+  data: VisitorData[];
+  isMobile: boolean;
+}) {
   console.log(data);
+  const now = new Date();
+  const currentHour = now.getHours();
+  console.log(typeof currentHour);
+  const newMericData = data.map((item) => ({
+    x: Number(item.hour),
+    y: item.count,
+  }));
+  console.log(typeof newMericData);
+  const maxCountHour = useMemo(() => {
+    return data.reduce((acc, cur) => (cur.count > acc.count ? cur : acc)).hour;
+  }, [data]);
+  const totalCount = useMemo(() => {
+    return data.reduce((acc, cur) => acc + cur.count, 0);
+  }, [data]);
+  console.log(totalCount, "totalCount");
+  console.log(maxCountHour, "maxCounterHour");
+  const filterData = useMemo(() => {
+    if (!isMobile) return data;
+    return data.filter(
+      (d) => d.hour >= currentHour - 1 && d.hour <= currentHour + 1
+    );
+  }, [currentHour, data, isMobile]);
+  console.log(filterData, "filterData");
   const options: ApexOptions = {
     legend: {
       show: true,
       position: "top",
       horizontalAlign: "center",
     },
-    colors: ["#465FFF", "#9cB9ff"],
+    colors: ["#465FFF"],
     chart: {
       fontFamily: "Pretendard, sans-serif",
       type: "bar",
       toolbar: {
-        show: true,
+        show: false,
+      },
+      zoom: {
+        enabled: false,
       },
     },
+
     stroke: {
       curve: "smooth",
       width: [1, 1],
@@ -59,20 +92,32 @@ export default function VisitorChart({ data }: { data: VisitorData[] }) {
     },
     dataLabels: {},
     tooltip: {
+      enabled: true,
       x: {
-        formatter: (val: unknown) => `${val as string} 시`,
+        formatter: (val: number) => `${val} 시`,
       },
     },
-    xaxis: { type: "category", categories: data.map((item) => item.hour) },
-    yaxis: {},
+    xaxis: {
+      type: "numeric",
+      tickAmount: isMobile ? 2 : 23,
+      labels: {
+        formatter: (val: number | string) => `${Math.round(Number(val))}시`,
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: (val: number) => `${val}명`,
+      },
+    },
   };
   const series = [
     {
-      name: " 방문자 수",
-      data: data.map((item) => {
+      name: "방문자 수",
+      data: filterData?.map((item) => {
         return {
-          x: item.hour || "00",
+          x: Number(item.hour) || 0,
           y: item.count || 0,
+          fillColor: item.hour === maxCountHour ? "#FF4560" : "#4e81ef",
         };
       }),
     },
@@ -80,7 +125,15 @@ export default function VisitorChart({ data }: { data: VisitorData[] }) {
   return (
     <>
       <div>
-        <ReactApexChart type="bar" options={options} series={series} />
+        <div className="text-center mb-2">
+          총 방문자 : <span className="font-semibold">{totalCount}명</span>
+        </div>
+        <ReactApexChart
+          type="bar"
+          options={options}
+          series={series}
+          height={350}
+        />
       </div>
     </>
   );
